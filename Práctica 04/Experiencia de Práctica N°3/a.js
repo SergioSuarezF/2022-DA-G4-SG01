189961b5d0a9f31f05b0766e93f535c59a7c626c
joelyamir <joel.pinto@ucsm.edu.pe>
2022-09-12 17:21:14 -0500

    funcion simple fix

--- Práctica 04/Experiencia de Práctica N°3/a.js
+++ Práctica 04/Experiencia de Práctica N°3/a.js
@@ -1,6 +1,13 @@
 // CIUDADANO DE PRIMERA CLASE 
 
+// Codigo
 const foo = () => {
     console.log("foobar");
 }
 foo();
+
+// Fixed
+const test = function() {
+    console.log("hello");
+}
+test();
