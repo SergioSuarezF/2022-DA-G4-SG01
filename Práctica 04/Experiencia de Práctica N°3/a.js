3228df912ac7c4f32a48fe68187d10a84dc83dd5
joelyamir <joel.pinto@ucsm.edu.pe>
2022-09-12 17:39:54 -0500

    Funcion como argumento fix

--- Práctica 04/Experiencia de Práctica N°3/a.js
+++ Práctica 04/Experiencia de Práctica N°3/a.js
@@ -20,3 +20,12 @@ function greeting(helloMessage, name) {
     console.log(helloMessage() + name);
 }
 greeting(sayHello, "JavaScript!");
+
+// Fixed
+function parte1() {
+    return "Hola";
+}
+function parte2(msg1, msg2) {
+    console.log(msg1() + msg2);
+}
+parte2(parte1, " Mundo!");
