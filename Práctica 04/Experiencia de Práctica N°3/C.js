a21394707d7a3438f66eb8f2d068c07d197e43e8
joelyamir <joel.pinto@ucsm.edu.pe>
2022-09-12 20:37:49 -0500

    Scope 2

--- Práctica 04/Experiencia de Práctica N°3/C.js
+++ Práctica 04/Experiencia de Práctica N°3/C.js
@@ -7,3 +7,13 @@ function exampleFunction() {
     console.log(x);
 }
 console.log(x);
+
+// Codigo
+const x = "declared outside function";
+exampleFunction();
+function exampleFunction() {
+    console.log("Inside function");
+    console.log(x);
+}
+console.log("Outside function");
+console.log(x);
