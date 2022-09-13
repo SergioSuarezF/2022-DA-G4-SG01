1a4b09990d6bb05b7b82ff55a871d29a1553b216
joelyamir <joel.pinto@ucsm.edu.pe>
2022-09-12 20:15:23 -0500

    Closure fix

--- Práctica 04/Experiencia de Práctica N°3/B.js
+++ Práctica 04/Experiencia de Práctica N°3/B.js
@@ -10,3 +10,16 @@ function makeFunc() {
 }
 const myFunc = makeFunc();
 myFunc();
+
+// Fixed
+function test() {
+    const text1 = 'Hello';
+    const text2 = ' World!';
+    function print() {
+        console.log(text1 + text2);
+    }
+    return print;
+}
+
+const text = test();
+text();
