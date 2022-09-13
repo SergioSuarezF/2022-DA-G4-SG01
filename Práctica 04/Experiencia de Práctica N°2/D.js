380c6113ac04bc822466571ad282e399320fa625
DiegoMendozaPolanco <101760867+DiegoMendozaPolanco@users.noreply.github.com>
2022-09-12 23:41:55 -0500

    Cambios de tipos de datos

---
+++ Práctica 04/Experiencia de Práctica N°2/D.js
@@ -0,0 +1,17 @@
+var answer = 42;
+answer = 'Gracias por todo el pescado...';
+console.log(answer)
+
+//parseInt("F", 16);
+/*parseInt("17", 8);
+parseInt("15", 10);
+parseInt(15.99, 10);
+parseInt("FXX123", 16);
+parseInt("1111", 2);
+parseInt("15*3", 10);
+parseInt("12", 13);
+parseInt("Hello", 8); // No es un número en absoluto
+parseInt("0x7", 10); // No es de base 10
+parseInt("546", 2); // Los dígitos no son válidos para representaciones binarias./*/
+
+console.log(parseInt("Hello", 8))
