23d56e592ee13ed667fcd127dc8c5cb079a229c0
joelyamir <joel.pinto@ucsm.edu.pe>
2022-09-12 20:34:06 -0500

    Scope

---
+++ Práctica 04/Experiencia de Práctica N°3/C.js
@@ -0,0 +1,9 @@
+// ÁMBITO DE FUNCIÓN
+
+// Codigo
+function exampleFunction() {
+    const x = "declared inside function";
+    console.log("Inside function");
+    console.log(x);
+}
+console.log(x);
