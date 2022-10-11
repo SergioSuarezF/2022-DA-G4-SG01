2a0f4ee238a373562730dd257007bd90d086df60
joelyamir <joel.pinto@ucsm.edu.pe>
2022-10-11 00:48:53 -0500

    fix

--- Práctica 07/Experiencias de Prácticas/Experiencia de Práctica N°3/Ejemplo 1/pages/recibos.js
+++ Práctica 07/Experiencias de Prácticas/Experiencia de Práctica N°3/Ejemplo 1/pages/recibos.js
@@ -10,11 +10,11 @@ const Recibos = () => {
   return (
     <main style={{ padding: "1rem 0" }}>
       <p>Recibos</p>
-      <li><Link to="1">Recibo 1</Link></li>
-      <li><Link to="2">Recibo 2</Link></li>
-      <li><Link to="3">Recibo 3</Link></li>
-      <li><Link to="100">Recibo 100</Link></li>
-      <li><Link to="5246">Recibo 5246</Link></li>
+      <li><Link to="1">Recibo #1</Link></li>
+      <li><Link to="2">Recibo #2</Link></li>
+      <li><Link to="3">Recibo #3</Link></li>
+      <li><Link to="100">Recibo #100</Link></li>
+      <li><Link to="5246">Recibo #5246</Link></li>
       <Outlet/>
     </main>
   );
