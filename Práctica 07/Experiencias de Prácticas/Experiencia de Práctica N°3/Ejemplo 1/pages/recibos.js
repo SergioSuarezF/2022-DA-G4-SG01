e4c8a098c4c683f81ec3bbaf7a65574fe02d1e48
joelyamir <joel.pinto@ucsm.edu.pe>
2022-10-11 00:48:27 -0500

    fix

--- Práctica 07/Experiencias de Prácticas/Experiencia de Práctica N°3/Ejemplo 1/pages/recibos.js
+++ Práctica 07/Experiencias de Prácticas/Experiencia de Práctica N°3/Ejemplo 1/pages/recibos.js
@@ -5,10 +5,16 @@ FECHA : 10/10/2022
 DESCRIPCIÓN :  React Router
 *******************************************************/
 import { Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
 const Recibos = () => {
   return (
     <main style={{ padding: "1rem 0" }}>
       <p>Recibos</p>
+      <li><Link to="1">Recibo 1</Link></li>
+      <li><Link to="2">Recibo 2</Link></li>
+      <li><Link to="3">Recibo 3</Link></li>
+      <li><Link to="100">Recibo 100</Link></li>
+      <li><Link to="5246">Recibo 5246</Link></li>
       <Outlet/>
     </main>
   );
