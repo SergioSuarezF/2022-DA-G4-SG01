6c98bc6dcd0a277e1a078e5e4852f6dab9eb29a2
SergioSuarezF <101756534+SergioSuarezF@users.noreply.github.com>
2022-11-16 01:39:01 -0500

    Ejercicio 1 - App básica y HTML con NodeJS

---
+++ Práctica 11/Ejercicios/Ejercicio 1 - NodeJS/Ejercicio Experiencia 1/ejexp1/src/servidor/servidor.js
@@ -0,0 +1,15 @@
+var http = require('http');
+
+var server = http.createServer();
+
+function mensaje(petic, resp){
+    resp.writeHead(200,{'content-type':'text/plain'});
+    resp.write('Hola Sergio Suarez');
+    resp.end();
+}
+
+server.on('request', mensaje);
+
+server.listen(3000, function(){
+    console.log('La aplicacion esta funcionando en el puerto 3000');
+});
