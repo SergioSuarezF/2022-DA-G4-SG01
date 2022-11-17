d32220e2f6ddaaf5b9ea8ebda2e3d76f94086eb2
SergioSuarezF <101756534+SergioSuarezF@users.noreply.github.com>
2022-11-17 00:02:30 -0500

    EJ 1 EXP 2

---
+++ Práctica 11/Ejercicios/Ejercicio 1 - NodeJS/Ejercicio experiencia 2/src/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'localhost',
+        user: 'Sergio Suarez',
+        password: 'celso2330',
+        database: 'db_links'
+    }
+
+};
